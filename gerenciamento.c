796c320240a1523121af204de45eeab4433cae6d
GiovanniWillian <giovanniwillian19.mafs@gmail.com>
2024-03-13 14:15:17 -0300

    Criação do gerenciamento.c, main.c e arquivo de cabeçalho.

---
+++ gerenciamento.c
@@ -0,0 +1,14 @@
+#include "gerenciamento.h"
+
+struct times{
+    char nome[20];
+    char pais[20];
+    char tecnico[20];
+    int quant_jogador;
+};
+
+struct evento{
+    char nome[20];
+    char tipo[20];
+    Times time;
+};
---
+++ main.c
@@ -0,0 +1,2 @@
+#include "gerenciamento.h"
+
